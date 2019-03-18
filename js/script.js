6cf85ea332839bf5f12797202201aefda60989d3
Veronika Palto <nikalalin@gmail.com>
2019-03-18 14:18:18 +0300

    Добавила файл скрипта

---
+++ js/script.js
@@ -0,0 +1,63 @@
+var letterButton = document.querySelector(".letter-button");
+
+var popup = document.querySelector(".modal-letter");
+var close = popup.querySelector(".modal-close");
+
+var form = popup.querySelector(".letter-form");
+var user = popup.querySelector("[name=name]");
+var email = popup.querySelector("[name=email]");
+var letter = popup.querySelector("[name=letter]");
+
+letterButton.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popup.classList.add("modal-show");
+  user.focus();
+});
+
+close.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popup.classList.remove("modal-show");
+  popup.classList.remove("modal-error");
+});
+
+form.addEventListener("submit", function (evt) {
+  if (!user.value || !email.value || !letter.value) {
+    evt.preventDefault();
+    popup.classList.remove("modal-error");
+    popup.offsetWidth = popup.offsetWidth;
+    popup.classList.add("modal-error");
+  }
+});
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    evt.preventDefault();
+    if (popup.classList.contains("modal-show")) {
+      popup.classList.remove("modal-show");
+      popup.classList.remove("modal-error");
+    }
+  }
+});
+
+var mapLink = document.querySelector(".contact-map-link");
+
+var mapPopup = document.querySelector(".modal-map");
+var mapClose = mapPopup.querySelector(".modal-close");
+
+mapLink.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  mapPopup.classList.add("modal-show");
+});
+
+mapClose.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  mapPopup.classList.remove("modal-show");
+});
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    if (mapPopup.classList.contains("modal-show")) {
+      mapPopup.classList.remove("modal-show");
+    }
+  }
+});
